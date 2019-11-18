4eb42ce24315d8080c09ca75d1b0527c97b5157e
rohit <rohitsharma9799569458@gmail.com>
2019-11-18 16:37:11 +0530

    second commit rohit

--- app/src/main/java/com/webdealsoft/Tiara/Activity/Home.java
+++ app/src/main/java/com/webdealsoft/Tiara/Activity/Home.java
@@ -104,7 +104,7 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
         Timer timer = new Timer();
         timer.scheduleAtFixedRate(new SliderTimer(), 2000, 3000);
 
-        //AdsLarge();
+        AdsLarge();
 
 
 
